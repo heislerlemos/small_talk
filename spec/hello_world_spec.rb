c81ac6a990c374a1462fdc7d011146709ac60e41
Heisler <heisler.lemos@ita.co.ao>
2019-09-23 15:22:31 +0100

    testing access the form and submit with rspec capybera

--- spec/hello_world_spec.rb
+++ spec/hello_world_spec.rb
@@ -3,6 +3,12 @@ require "rails_helper"
 RSpec.describe 'Hello World', type: :feature do  
 	scenario  'index page' do 
 		visit root_path
+		fill_in 'Email', with: 'heisler.lemos@ita.co.ao'
+		fill_in 'Password', with: '************'
+		click_button 
+		
+
+
 		expect(page).to have_content('welcome to small talk')
 		
 
